3875e7499339ead7413efd759f061a8a995a81eb
David Bond <david.bond@panoramicdata.com>
2019-10-21 12:54:13 +0100

    Added reserved keywords True (=true) and False (=false)

--- PanoramicData.NCalcExtensions/ExtendedExpression.cs
+++ PanoramicData.NCalcExtensions/ExtendedExpression.cs
@@ -14,6 +14,16 @@ namespace PanoramicData.NCalcExtensions
 				throw new InvalidOperationException("You may not set a parameter called 'null', as it is a reserved keyword.");
 			}
 			Parameters["null"] = null;
+			if (Parameters.ContainsKey("True"))
+			{
+				throw new InvalidOperationException("You may not set a parameter called 'True', as it is a reserved keyword.");
+			}
+			Parameters["True"] = true;
+			if (Parameters.ContainsKey("False"))
+			{
+				throw new InvalidOperationException("You may not set a parameter called 'False', as it is a reserved keyword.");
+			}
+			Parameters["False"] = false;
 		}
 	}
 }
