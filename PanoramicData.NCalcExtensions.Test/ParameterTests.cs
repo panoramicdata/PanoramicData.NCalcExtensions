773ae6ee1dd35f36b022504676b6e83c21cc32a4
David Bond <david.bond@panoramicdata.com>
2020-02-18 16:24:49 +0000

    Added a unit test

---
+++ PanoramicData.NCalcExtensions.Test/ParameterTests.cs
@@ -0,0 +1,15 @@
+﻿using Xunit;
+
+namespace PanoramicData.NCalcExtensions.Test
+{
+	public class ParameterTests
+	{
+		[Fact]
+		public void SquareBracketParameter_Succeeds()
+		{
+			var expression = new ExtendedExpression("[a.b]");
+			expression.Parameters["a.b"] = "AAAA";
+			Assert.Equal("AAAA", expression.Evaluate() as string);
+		}
+	}
+}
