779f70664b837320e18957d462b7e91a2108a6d1
David Bond <david.bond@panoramicdata.com>
2020-02-25 23:59:33 +0000

    Added a test

--- PanoramicData.NCalcExtensions.Test/DateTimeAsEpochMsTests.cs
+++ PanoramicData.NCalcExtensions.Test/DateTimeAsEpochMsTests.cs
@@ -11,5 +11,15 @@ namespace PanoramicData.NCalcExtensions.Test
 			const long desiredDateTimeEpochMs = 1562025600000;
 			Assert.Equal(desiredDateTimeEpochMs, result);
 		}
+
+		[Fact]
+		public void WeirdShit_Succeeds()
+		{
+			var expression = new ExtendedExpression("1 > dateTimeAsEpochMs([connectMagic.systemItem.sys_updated_on], 'yyyy-MM-dd HH:mm:ss')");
+			expression.Parameters.Add("connectMagic.systemItem.sys_updated_on", "2018-01-01 01:01:01");
+			var result = expression.Evaluate();
+			const long desiredDateTimeEpochMs = 1514768461000;
+			Assert.Equal(false, result);
+		}
 	}
 }
