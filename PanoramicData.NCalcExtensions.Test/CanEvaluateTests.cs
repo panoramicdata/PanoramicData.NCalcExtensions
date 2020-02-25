fc324fe26559a4053641d5869132793bd34c82e0
David Bond <david.bond@panoramicdata.com>
2020-02-25 10:55:03 +0000

    Added canEvaluate()

---
+++ PanoramicData.NCalcExtensions.Test/CanEvaluateTests.cs
@@ -0,0 +1,21 @@
+﻿using Xunit;
+
+namespace PanoramicData.NCalcExtensions.Test
+{
+	public class CanEvaluateTests
+	{
+		[Fact]
+		public void CanEvaluate_Example1_Succeeds()
+		{
+			var expression = new ExtendedExpression("canEvaluate(nonExistent)");
+			Assert.False(expression.Evaluate() as bool?);
+		}
+
+		[Fact]
+		public void CanEvaluate_Example2_Succeeds()
+		{
+			var expression = new ExtendedExpression("canEvaluate(1)");
+			Assert.True(expression.Evaluate() as bool?);
+		}
+	}
+}
