cb329ff721354c1425ddb0e75558cbb4a524602f
David Bond <david.bond@panoramicdata.com>
2020-06-07 16:44:48 +0100

    Code tidy

---
+++ PanoramicData.NCalcExtensions.Test/GlobalSuppressions.cs
@@ -0,0 +1,13 @@
+﻿// This file is used by Code Analysis to maintain SuppressMessage
+// attributes that are applied to this project.
+// Project-level suppressions either have no target or are given
+// a specific target and scoped to a namespace, type, member, etc.
+
+using System.Diagnostics.CodeAnalysis;
+
+[assembly: SuppressMessage(
+	 "Globalization",
+	 "CA1303:Do not pass literals as localized parameters",
+	 Justification = "The library will not be localized",
+	 Scope = "namespaceanddescendantsmember",
+	 Target = "PanoramicData.NCalcExtensions.Test")]
