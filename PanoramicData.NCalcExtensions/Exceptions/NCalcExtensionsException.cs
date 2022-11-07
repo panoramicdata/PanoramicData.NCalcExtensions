af625c67b9c77add29bda57cb841f33982c2b964
David Bond <david.bond@panoramicdata.com>
2022-11-07 23:20:12 +0000

    Codacity code tidy

--- PanoramicData.NCalcExtensions/Exceptions/NCalcExtensionsException.cs
+++ PanoramicData.NCalcExtensions/Exceptions/NCalcExtensionsException.cs
@@ -2,10 +2,6 @@
 
 public class NCalcExtensionsException : Exception
 {
-	public NCalcExtensionsException() : base()
-	{
-	}
-
 	public NCalcExtensionsException(string message) : base(message)
 	{
 	}
