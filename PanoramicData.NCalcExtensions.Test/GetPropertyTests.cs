bb303090382160a99629bcde082c977707123034
David Bond <david.bond@panoramicdata.com>
2023-01-24 14:25:46 +0000

    Added getProperty(), including JObject support.

--- PanoramicData.NCalcExtensions.Test/GetPropertyTests.cs
+++ PanoramicData.NCalcExtensions.Test/GetPropertyTests.cs
@@ -11,4 +11,13 @@ public class GetPropertyTests
 		result.Should().BeOfType<int>();
 		result.Should().Be(year);
 	}
+
+	[Fact]
+	public void GetProperty_FromJObject()
+	{
+		var expression = new ExtendedExpression($"getProperty(parse('jObject', '{{ \"A\": 1, \"B\": 2 }}'), 'B')");
+		var result = expression.Evaluate();
+		result.Should().BeOfType<int>();
+		result.Should().Be(2);
+	}
 }
