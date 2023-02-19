2bef42eea39212da9c9ffba8ea4f7a74648085cf
David Bond <david.bond@panoramicdata.com>
2023-02-19 18:23:57 +0000

    Added nullCoalesce() tests

--- PanoramicData.NCalcExtensions.Test/NullCoalesceTests.cs
+++ PanoramicData.NCalcExtensions.Test/NullCoalesceTests.cs
@@ -12,9 +12,26 @@ public class NullCoalesceTests
 	[InlineData("null", null)]
 	[InlineData("1", 1)]
 	[InlineData("", null)]
-	public void Parse_IncorrectParameterCountOrType_Throws(string parameters, object? expectedValue)
+	public void Parse_GoodValues_Succeeds(string parameters, object? expectedValue)
 	{
 		var expression = new ExtendedExpression($"nullCoalesce({parameters})");
+		var result = expression.Evaluate();
+		result.Should().Be(expectedValue);
+	}
+
+	[Theory]
+	[InlineData(null, null, null, null)]
+	[InlineData("A", null, null, "A")]
+	[InlineData("A", "B", "C", "A")]
+	[InlineData(null, "B", null, null)]
+	[InlineData(null, null, "C", null)]
+	[InlineData(null, "B", "C", "B.C")]
+	public void Parse_ComplexInput_Succeeds(object? a, object? b, object? c, string? expectedValue)
+	{
+		var expression = new ExtendedExpression($"nullCoalesce(a, if(!isNull(b) && !isNull(c), b + '.' + c, null))");
+		expression.Parameters.Add("a", a);
+		expression.Parameters.Add("b", b);
+		expression.Parameters.Add("c", c);
 		var result = expression.Evaluate();
 		result.Should().Be(expectedValue);
 	}
