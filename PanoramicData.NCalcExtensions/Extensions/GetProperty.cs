1b933ef72b9f9cb60b85008b93fe43267df790b4
Nick Young <nick.young@panoramicdata.com>
2023-06-12 17:19:03 +0100

    Codacy Fix

--- PanoramicData.NCalcExtensions/Extensions/GetProperty.cs
+++ PanoramicData.NCalcExtensions/Extensions/GetProperty.cs
@@ -16,34 +16,33 @@ internal static class GetProperty
 			throw new FormatException($"{ExtensionFunction.GetProperty}() requires two parameters.", e);
 		}
 
-		switch (value)
+		if (value is JObject jObject)
 		{
-			case JObject jObject:
-				var jToken = jObject[property];
+			var jToken = jObject[property];
 
-				functionArgs.Result = (jToken?.Type) switch
-				{
-					null or JTokenType.Null or JTokenType.Undefined => null,
-					JTokenType.Object => jToken.ToObject<JObject>(),
-					JTokenType.Array => jToken.ToObject<JArray>(),
-					JTokenType.Constructor => jToken.ToObject<JConstructor>(),
-					JTokenType.Property => jToken.ToObject<JProperty>(),
-					JTokenType.Comment => jToken.ToObject<JValue>(),
-					JTokenType.Integer => jToken.ToObject<int>(),
-					JTokenType.Float => jToken.ToObject<float>(),
-					JTokenType.String => jToken.ToObject<string>(),
-					JTokenType.Boolean => jToken.ToObject<bool>(),
-					JTokenType.Date => jToken.ToObject<DateTime>(),
-					JTokenType.Raw or JTokenType.Bytes => jToken.ToObject<JValue>(),
-					JTokenType.Guid => jToken.ToObject<Guid>(),
-					_ => throw new NotSupportedException("Unsupported JTokenType: " + jToken.Type),
-				};
-				break;
-			default:
-				var type = value.GetType();
-				var propertyInfo = type.GetProperty(property) ?? throw new FormatException($"Could not find property {property} on type {type.Name}");
-				functionArgs.Result = propertyInfo.GetValue(value);
-				break;
+			functionArgs.Result = jToken?.Type switch
+			{
+				null or JTokenType.Null or JTokenType.Undefined => null,
+				JTokenType.Object => jToken.ToObject<JObject>(),
+				JTokenType.Array => jToken.ToObject<JArray>(),
+				JTokenType.Constructor => jToken.ToObject<JConstructor>(),
+				JTokenType.Property => jToken.ToObject<JProperty>(),
+				JTokenType.Comment => jToken.ToObject<JValue>(),
+				JTokenType.Integer => jToken.ToObject<int>(),
+				JTokenType.Float => jToken.ToObject<float>(),
+				JTokenType.String => jToken.ToObject<string>(),
+				JTokenType.Boolean => jToken.ToObject<bool>(),
+				JTokenType.Date => jToken.ToObject<DateTime>(),
+				JTokenType.Raw or JTokenType.Bytes => jToken.ToObject<JValue>(),
+				JTokenType.Guid => jToken.ToObject<Guid>(),
+				_ => throw new NotSupportedException("Unsupported JTokenType: " + jToken.Type)
+			};
+		}
+		else
+		{
+			var type = value.GetType();
+			var propertyInfo = type.GetProperty(property) ?? throw new FormatException($"Could not find property {property} on type {type.Name}");
+			functionArgs.Result = propertyInfo.GetValue(value);
 		}
 	}
 }
