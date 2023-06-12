5dfb3e0f4a1f7670c7641f79225bbad797b0cbc5
Nick Young <nick.young@panoramicdata.com>
2023-06-12 16:32:05 +0100

    Codacy Cleanup

--- PanoramicData.NCalcExtensions/Extensions/GetProperty.cs
+++ PanoramicData.NCalcExtensions/Extensions/GetProperty.cs
@@ -4,17 +4,8 @@ internal static class GetProperty
 {
 	internal static void Evaluate(FunctionArgs functionArgs)
 	{
-		object value;
-		string property;
-		try
-		{
-			value = functionArgs.Parameters[0].Evaluate();
-			property = (string)functionArgs.Parameters[1].Evaluate();
-		}
-		catch (Exception e)
-		{
-			throw new FormatException($"{ExtensionFunction.GetProperty}() requires two parameters.", e);
-		}
+		object value = functionArgs.Parameters[0].Evaluate();
+		string property = (string)functionArgs.Parameters[1].Evaluate();
 
 		switch (value)
 		{
--- PanoramicData.NCalcExtensions/Extensions/Max.cs
+++ PanoramicData.NCalcExtensions/Extensions/Max.cs
@@ -10,36 +10,15 @@ internal static class Max
 
 		if (functionArgs.Parameters.Length == 1)
 		{
-			functionArgs.Result = originalList switch
-			{
-				null => null,
-				IEnumerable<byte> list => list.Cast<int>().Max(),
-				IEnumerable<byte?> list => list.DefaultIfEmpty(null).Max(),
-				IEnumerable<short> list => list.Cast<int>().Max(),
-				IEnumerable<short?> list => list.DefaultIfEmpty(null).Max(),
-				IEnumerable<int> list => list.Max(),
-				IEnumerable<int?> list => list.DefaultIfEmpty(null).Max(),
-				IEnumerable<long> list => list.Max(),
-				IEnumerable<long?> list => list.DefaultIfEmpty(null).Max(),
-				IEnumerable<float> list => list.Max(),
-				IEnumerable<float?> list => list.DefaultIfEmpty(null).Max(),
-				IEnumerable<double> list => list.Max(),
-				IEnumerable<double?> list => list.DefaultIfEmpty(null).Max(),
-				IEnumerable<decimal> list => list.Max(),
-				IEnumerable<decimal?> list => list.DefaultIfEmpty(null).Max(),
-				IEnumerable<string?> list => list.DefaultIfEmpty(null).Max(),
-				IEnumerable<object?> list when list.All(x => x is string or null) => list.DefaultIfEmpty(null).Max(x => x as string),
-				IEnumerable<object?> list => GetMax(list),
-				_ => throw new FormatException($"First {ExtensionFunction.Max} parameter must be an IEnumerable of a numeric or string type if only on parameter is present.")
-			};
+			functionArgs.Result = originalList;
 			return;
 		}
 
 		var predicate = functionArgs.Parameters[1].Evaluate() as string
-			?? throw new FormatException($"Second {ExtensionFunction.Max} parameter must be a string.");
+			 ?? throw new FormatException($"Second {ExtensionFunction.Max} parameter must be a string.");
 
 		var lambdaString = functionArgs.Parameters[2].Evaluate() as string
-			?? throw new FormatException($"Third {ExtensionFunction.Max} parameter must be a string.");
+			 ?? throw new FormatException($"Third {ExtensionFunction.Max} parameter must be a string.");
 
 		var lambda = new Lambda(predicate, lambdaString, new());
 
@@ -59,9 +38,10 @@ internal static class Max
 			IEnumerable<double?> list => list.Max(value => (double?)lambda.Evaluate(value)),
 			IEnumerable<decimal> list => list.Max(value => (decimal?)lambda.Evaluate(value)),
 			IEnumerable<decimal?> list => list.Max(value => (decimal?)lambda.Evaluate(value)),
-			IEnumerable<string?> list => list.Max(value => (string)lambda.Evaluate(value)),
+			IEnumerable<string?> list => list.Max(value => (string?)lambda.Evaluate(value)),
 			_ => throw new FormatException($"First {ExtensionFunction.Max} parameter must be an IEnumerable of a numeric type.")
 		};
+
 	}
 
 	private static double GetMax(IEnumerable<object?> objectList)
