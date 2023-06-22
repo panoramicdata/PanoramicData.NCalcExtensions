c192cdc18ea1b0559fd85604e4bf8201a41afb0f
David Bond <david.bond@panoramicdata.com>
2023-06-22 18:42:38 +0100

    Added missing isNan() tests.

---
+++ PanoramicData.NCalcExtensions.Test/IsNanTests.cs
@@ -0,0 +1,25 @@
+﻿namespace PanoramicData.NCalcExtensions.Test;
+
+public class IsNanTests
+{
+	[Fact]
+	public void IsNan_Example1_Succeeds()
+	{
+		var expression = new ExtendedExpression("isNan(1)");
+		expression.Evaluate().Should().Be(false);
+	}
+
+	[Fact]
+	public void IsNan_Example2_Succeeds()
+	{
+		var expression = new ExtendedExpression("isNan(null)");
+		expression.Evaluate().Should().Be(true);
+	}
+
+	[Fact]
+	public void IsNan_Example3_Succeeds()
+	{
+		var expression = new ExtendedExpression("isNan('text')");
+		expression.Evaluate().Should().Be(true);
+	}
+}
