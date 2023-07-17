9563547e96ce96aa82ff91445daeb3786f0cfa29
Ecstatik <johankool2003@hotmail.co.uk>
2023-07-17 13:40:44 +0100

    Added Contains,EndsWith and Humanize tests

---
+++ PanoramicData.NCalcExtensions.Test/ContainsTest.cs
@@ -0,0 +1,19 @@
+﻿namespace PanoramicData.NCalcExtensions.Test;
+public class ContainsTest
+{
+	[Theory]
+	[InlineData("contains('abc', 'a')", true)]
+	[InlineData("contains('abc', 'b')", true)]
+	[InlineData("contains('abc', 'c')", true)]
+	[InlineData("contains('abc', 'd')", false)]
+	[InlineData("contains('abc', 'ab')", true)]
+	[InlineData("contains('abc', 'bc')", true)]
+	[InlineData("contains('123', '12')", true)]
+	[InlineData("contains('123', '13')", false)]
+	public void Contains_Succeeds(string expressionText, bool expected)
+	{
+		var expression = new ExtendedExpression(expressionText);
+		var result = expression.Evaluate();
+		Assert.Equal(expected, result);
+	}
+}
---
+++ PanoramicData.NCalcExtensions.Test/HumanizeTests.cs
@@ -0,0 +1,17 @@
+﻿namespace PanoramicData.NCalcExtensions.Test;
+public class HumanizeTests
+{
+	[Theory]
+	[InlineData("3600000", "Milliseconds", "1 hour")]
+	[InlineData("3600", "Seconds", "1 hour")]
+	[InlineData("60", "Minutes", "1 hour")]
+	[InlineData("1", "Hours", "1 hour")]
+	[InlineData("1", "Days", "1 day")]
+	[InlineData("1", "Weeks", "7 days")]
+	public void Humanize_Succeeds(string expressionText,string datatype, string expected)
+	{
+		var expression = new ExtendedExpression($"humanize({expressionText},'{datatype}')");
+		var result = expression.Evaluate();
+		Assert.Equal(expected, result);
+	}
+}
