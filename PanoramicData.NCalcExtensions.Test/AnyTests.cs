e2acf09f5d5b70d304b341419e38bf1fe9507db1
David Bond <david.bond@panoramicdata.com>
2024-01-28 14:13:34 +0000

    Added any(list()) and improved any() error handling

--- PanoramicData.NCalcExtensions.Test/AnyTests.cs
+++ PanoramicData.NCalcExtensions.Test/AnyTests.cs
@@ -14,4 +14,32 @@ public class AnyTests : NCalcTest
 
 		result.Should().Be(expected);
 	}
+
+	[Fact]
+	public void Any_Items_ReturnsTrue()
+	{
+		var expression = new ExtendedExpression($"any(list(1, 2, 3))");
+		var result = expression.Evaluate();
+		result.Should().Be(true);
+	}
+
+	[Fact]
+	public void Any_NoItems_ReturnsFalse()
+	{
+		var expression = new ExtendedExpression($"any(list())");
+		var result = expression.Evaluate();
+		result.Should().Be(false);
+	}
+
+	[Theory]
+	[InlineData("null")]
+	[InlineData("'a'")]
+	[InlineData("list(1), 1")]
+	[InlineData("list(1), null, 1")]
+	[InlineData("list(1), 'a', 1")]
+	[InlineData("list(1), 'a', null")]
+	[InlineData("1, 2")]
+	[InlineData("1, 2, 3, 4")]
+	public void Any_BadParameters_ThrowsException(string parametersString)
+		=> new ExtendedExpression($"any({parametersString})").Invoking(x => x.Evaluate()).Should().ThrowExactly<FormatException>();
 }
--- PanoramicData.NCalcExtensions/Extensions/Any.cs
+++ PanoramicData.NCalcExtensions/Extensions/Any.cs
@@ -6,22 +6,37 @@ internal static class Any
 {
 	internal static void Evaluate(FunctionArgs functionArgs)
 	{
-		var list = functionArgs.Parameters[0].Evaluate() as IEnumerable<object?>
-			?? throw new FormatException($"First {ExtensionFunction.Any} parameter must be an IEnumerable.");
+		switch (functionArgs.Parameters.Length)
+		{
+			case 0:
+				throw new FormatException($"At least one parameter must be provided for {ExtensionFunction.Any}.");
+			case 1:
+				var list = functionArgs.Parameters[0].Evaluate() as IEnumerable<object?>
+					?? throw new FormatException($"First {ExtensionFunction.Any} parameter must be an IEnumerable.");
 
-		var predicate = functionArgs.Parameters[1].Evaluate() as string
-			?? throw new FormatException($"Second {ExtensionFunction.Any} parameter must be a string.");
+				functionArgs.Result = list.Any();
+				return;
+			case 3:
+				list = functionArgs.Parameters[0].Evaluate() as IEnumerable<object?>
+					?? throw new FormatException($"First {ExtensionFunction.Any} parameter must be an IEnumerable.");
 
-		var lambdaString = functionArgs.Parameters[2].Evaluate() as string
-			?? throw new FormatException($"Third {ExtensionFunction.Any} parameter must be a string.");
+				var predicate = functionArgs.Parameters[1].Evaluate() as string
+					?? throw new FormatException($"Second {ExtensionFunction.Any} parameter must be a string.");
 
-		var lambda = new Lambda(predicate, lambdaString, functionArgs.Parameters[0].Parameters);
+				var lambdaString = functionArgs.Parameters[2].Evaluate() as string
+					?? throw new FormatException($"Third {ExtensionFunction.Any} parameter must be a string.");
 
-		functionArgs.Result = list
-			.Any(value =>
-			{
-				var result = lambda.Evaluate(value) as bool?;
-				return result == true;
-			});
+				var lambda = new Lambda(predicate, lambdaString, functionArgs.Parameters[0].Parameters);
+
+				functionArgs.Result = list
+					.Any(value =>
+					{
+						var result = lambda.Evaluate(value) as bool?;
+						return result == true;
+					});
+				return;
+			default:
+				throw new FormatException($"{ExtensionFunction.Any} takes either 1 or 3 parameters.");
+		}
 	}
 }
