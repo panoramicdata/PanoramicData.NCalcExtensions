81da8c480791dde9a6343ca7740e122db18e8138
David Bond <david.bond@panoramicdata.com>
2024-02-27 16:56:07 +0000

    Code tidy

--- PanoramicData.NCalcExtensions.Test/CountByTests.cs
+++ PanoramicData.NCalcExtensions.Test/CountByTests.cs
@@ -5,7 +5,7 @@ public class CountByTests
 	[Theory]
 	[InlineData("countBy(list(1, 2, 2, 3, 3, 3, 4), 'n', 'toLower(toString(n > 1))')", "{\"false\":1,\"true\":6}")]
 	[InlineData("countBy(list(1, 2, 2, 3, 3, 3, 4), 'n', 'toString(n)')", "{\"1\":1,\"2\":2,\"3\":3,\"4\":1}")]
-	public void Count_ReturnsExpectedResult(string expressionText, string expectedResult)
+	public void CountBy_ReturnsExpectedResult(string expressionText, string expectedResult)
 	{
 		var expression = new ExtendedExpression(expressionText);
 
