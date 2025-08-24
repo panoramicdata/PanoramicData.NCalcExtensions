025069fbeb4d82ff3fd1ceb7b945ffbee4b93b6d
Roland Banks <roland.banks@panoramicdata.com>
2025-08-24 11:35:38 +0700

    Fixed an issue with ItemAtIndex, where it would fail if the index parameter was an int64, e.g. this would fail (but now succeeds) itemAtIndex(list(1, 2, 3, 4), cast(1, 'System.Int64')). If an Int64 value large than int.MaxValue is used for the index, the format exception is improved to say the value is outside the valid values (0 - int.MaxValue).

--- PanoramicData.NCalcExtensions.Test/ItemAtIndexTests.cs
+++ PanoramicData.NCalcExtensions.Test/ItemAtIndexTests.cs
@@ -18,9 +18,20 @@ public class ItemAtIndexTests : NCalcTest
 	public void ItemAtIndex_ReturnsExpected(string expression, object? expectedOutput)
 		=> new ExtendedExpression(expression).Evaluate().Should().Be(expectedOutput);
 
-	[Theory]
 	[InlineData("itemAtIndex(list(1, 2, 3, 4, 5), 1)", 2)]
-	[InlineData("itemAtIndex(list(1, 2, 3, 4, 5), 1)", 2L)]
-	public void ItemAtIndexWithInt32AndInt64Index_ReturnsExpected(string expression, object? expectedOutput)
+	public void ItemAtIndexWithListInts_ReturnsExpected(string expression, object? expectedOutput)
 		=> new ExtendedExpression(expression).Evaluate().Should().Be(expectedOutput);
+
+	[Theory]
+	[InlineData("itemAtIndex(list(1, 2, 3, 4), cast(1, 'System.Int64'))", 2)]
+	public void ItemAtIndexWithInt64IndexValue_ReturnsExpected(string expression, object? expectedOutput)
+		=> new ExtendedExpression(expression).Evaluate().Should().Be(expectedOutput);
+
+	[Theory]
+	[InlineData($"itemAtIndex(list(1, 2, 3, 4), 2147483648)")]
+	public void ItemAtIndexWithInt64IndexValueTooLarge_ThrowsException(string expression)
+		=> new ExtendedExpression(expression)
+			.Invoking(e => e.Evaluate())
+			.Should()
+			.ThrowExactly<FormatException>();
 }
--- PanoramicData.NCalcExtensions/Extensions/ItemAtIndex.cs
+++ PanoramicData.NCalcExtensions/Extensions/ItemAtIndex.cs
@@ -28,15 +28,26 @@ internal static class ItemAtIndex
 		try
 		{
 			input = (IList)functionArgs.Parameters[0].Evaluate();
-			index = (int)functionArgs.Parameters[1].Evaluate();
+
+			// Use Convert.ToInt32 instead of (int) cast because otherwise this would fail with
+			// an InvalidCastException when doing this: itemAtIndex(list(1, 2, 3, 4), cast(1, 'System.Int64'))
+			try
+			{
+				index = Convert.ToInt32(functionArgs.Parameters[1].Evaluate());
+			}
+			catch (OverflowException)
+			{
+				throw new FormatException($"{ExtensionFunction.ItemAtIndex}() index parameter is outside the range of valid integer values (0 - {int.MaxValue}).");
+			}
+
 			if (index < 0)
 			{
-				throw new FormatException($"{ExtensionFunction.ItemAtIndex}() requires two parameters.  The first should be an IList and the second should be a non-negative integer.");
+				throw new FormatException($"{ExtensionFunction.ItemAtIndex}() requires two parameters. The first should be an IList and the second should be a non-negative integer.");
 			}
 		}
 		catch (Exception e) when (e is not NCalcExtensionsException or FormatException)
 		{
-			throw new FormatException($"{ExtensionFunction.ItemAtIndex}() requires two parameters.  The first should be an IList and the second should be a non-negative integer.");
+			throw new FormatException($"{ExtensionFunction.ItemAtIndex}() requires two parameters. The first should be an IList and the second should be a non-negative integer.");
 		}
 
 		functionArgs.Result = input[index];
