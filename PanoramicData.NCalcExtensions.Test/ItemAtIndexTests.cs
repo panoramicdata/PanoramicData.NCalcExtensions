c1842ccfc593ba45ef6508e30b0268d11819820a
Roland Banks <roland.banks@panoramicdata.com>
2025-08-25 15:32:10 +0700

    Added additional unit tests for ItemAtIndex.

--- PanoramicData.NCalcExtensions.Test/ItemAtIndexTests.cs
+++ PanoramicData.NCalcExtensions.Test/ItemAtIndexTests.cs
@@ -1,4 +1,6 @@
-﻿namespace PanoramicData.NCalcExtensions.Test;
+﻿using System.Collections.Generic;
+
+namespace PanoramicData.NCalcExtensions.Test;
 
 public class ItemAtIndexTests : NCalcTest
 {
@@ -34,4 +36,48 @@ public class ItemAtIndexTests : NCalcTest
 			.Invoking(e => e.Evaluate())
 			.Should()
 			.ThrowExactly<FormatException>();
+
+	[Fact]
+	public void ItemAtIndexWith2ListsAndMultiply_Succeeds()
+	{
+		var result = new ExtendedExpression("itemAtIndex(list(1,2,3,4),0) * itemAtIndex(list(1,2,3,4),1)")
+			.Evaluate();
+
+		result.Should().NotBeNull();
+		result.Should().Be(2);
+	}
+
+	[Fact]
+	public void ItemAtIndexSelectWithListTable_Succeeds()
+	{
+		var result = new ExtendedExpression("select(list(list(1,2),list(2,3),list(3,4)), 'X', 'itemAtIndex(X,0) * itemAtIndex(X,1)')")
+			.Evaluate();
+
+		result.Should().NotBeNull();
+		result.Should().BeEquivalentTo(new List<int> { 2, 6, 12 });
+	}
+
+	[Fact]
+	public void ItemAtIndexSelectWithJArrayTableAndCast_Succeeds()
+	{
+		var result = new ExtendedExpression(
+				@$"select
+				(
+					jArray
+					(
+						jArray(1,2),
+						jArray(2,3),
+						jArray(3,4)
+					),
+					'X',
+					'
+						cast(itemAtIndex(X,0), \'System.Int32\') *
+						cast(itemAtIndex(X,1), \'System.Int32\')
+					'
+				)")
+			.Evaluate();
+
+		result.Should().NotBeNull();
+		result.Should().BeEquivalentTo(new List<int> { 2, 6, 12 });
+	}
 }
