d98f7e972fc72b09947b135a9073ff59f3986cfc
David Bond <david.bond@panoramicdata.com>
2025-09-10 12:25:49 +0100

    Added null operator tests, including one deliberately-failing test.

---
+++ PanoramicData.NCalcExtensions.Test/NullOperatorTests.cs
@@ -0,0 +1,14 @@
+﻿namespace PanoramicData.NCalcExtensions.Test;
+
+public class NullOperatorTests : NCalcTest
+{
+	[Theory]
+	[InlineData("null")]
+	[InlineData("null + 1")]
+	[InlineData("null - 1")]
+	[InlineData("null * 1")]
+	[InlineData("null / 1")]
+	[InlineData("null % 1")]
+	public void NullOutputTest_Succeeds(string expressionThatShouldResultInNull)
+		=> new ExtendedExpression(expressionThatShouldResultInNull).Evaluate().Should().BeNull();
+}
