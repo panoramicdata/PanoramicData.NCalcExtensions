8fdcf0a4cda77bab6b3b07f78031ad83df954ba0
David Bond <david.bond@panoramicdata.com>
2025-11-05 15:03:55 +0000

    Codacy fixes

--- PanoramicData.NCalcExtensions/Extensions/Parameters.cs
+++ PanoramicData.NCalcExtensions/Extensions/Parameters.cs
@@ -21,11 +21,10 @@ internal static class Parameters
 		[CallerMemberName] string callerName = "")
 	{
 		CheckParameterCount(2, 2, args.Parameters, callerName);
-		var index = 0;
 
 		return new Tuple<T1, T2>(
-			(T1?)args.Parameters[index++].Evaluate() ?? throw new FormatException($"{callerName} first argument should be a {typeof(T1).Name}."),
-			(T2?)args.Parameters[index++].Evaluate() ?? throw new FormatException($"{callerName} second argument should be a {typeof(T2).Name}.")
+			(T1?)args.Parameters[0].Evaluate() ?? throw new FormatException($"{callerName} first argument should be a {typeof(T1).Name}."),
+			(T2?)args.Parameters[1].Evaluate() ?? throw new FormatException($"{callerName} second argument should be a {typeof(T2).Name}.")
 			);
 	}
 
@@ -34,12 +33,11 @@ internal static class Parameters
 		[CallerMemberName] string callerName = "")
 	{
 		CheckParameterCount(3, 3, args.Parameters, callerName);
-		var index = 0;
 
 		return new Tuple<T1, T2, T3>(
-			(T1?)args.Parameters[index++].Evaluate() ?? throw new FormatException($"{callerName} first argument should be a {typeof(T1).Name}."),
-			(T2?)args.Parameters[index++].Evaluate() ?? throw new FormatException($"{callerName} second argument should be a {typeof(T2).Name}."),
-			(T3?)args.Parameters[index++].Evaluate() ?? throw new FormatException($"{callerName} third argument should be a {typeof(T3).Name}.")
+			(T1?)args.Parameters[0].Evaluate() ?? throw new FormatException($"{callerName} first argument should be a {typeof(T1).Name}."),
+			(T2?)args.Parameters[1].Evaluate() ?? throw new FormatException($"{callerName} second argument should be a {typeof(T2).Name}."),
+			(T3?)args.Parameters[2].Evaluate() ?? throw new FormatException($"{callerName} third argument should be a {typeof(T3).Name}.")
 			);
 	}
 
@@ -48,13 +46,12 @@ internal static class Parameters
 		[CallerMemberName] string callerName = "")
 	{
 		CheckParameterCount(4, 4, args.Parameters, callerName);
-		var index = 0;
 
 		return new Tuple<T1, T2, T3, T4>(
-			(T1?)args.Parameters[index++].Evaluate() ?? throw new FormatException($"{callerName} first argument should be a {typeof(T1).Name}."),
-			(T2?)args.Parameters[index++].Evaluate() ?? throw new FormatException($"{callerName} second argument should be a {typeof(T2).Name}."),
-			(T3?)args.Parameters[index++].Evaluate() ?? throw new FormatException($"{callerName} third argument should be a {typeof(T3).Name}."),
-			(T4?)args.Parameters[index++].Evaluate() ?? throw new FormatException($"{callerName} fourth argument should be a {typeof(T4).Name}.")
+			(T1?)args.Parameters[0].Evaluate() ?? throw new FormatException($"{callerName} first argument should be a {typeof(T1).Name}."),
+			(T2?)args.Parameters[1].Evaluate() ?? throw new FormatException($"{callerName} second argument should be a {typeof(T2).Name}."),
+			(T3?)args.Parameters[2].Evaluate() ?? throw new FormatException($"{callerName} third argument should be a {typeof(T3).Name}."),
+			(T4?)args.Parameters[3].Evaluate() ?? throw new FormatException($"{callerName} fourth argument should be a {typeof(T4).Name}.")
 			);
 	}
 
@@ -63,14 +60,13 @@ internal static class Parameters
 		[CallerMemberName] string callerName = "")
 	{
 		CheckParameterCount(5, 5, args.Parameters, callerName);
-		var index = 0;
 
 		return new Tuple<T1, T2, T3, T4, T5>(
-			(T1?)args.Parameters[index++].Evaluate() ?? throw new FormatException($"{callerName} first argument should be a {typeof(T1).Name}."),
-			(T2?)args.Parameters[index++].Evaluate() ?? throw new FormatException($"{callerName} second argument should be a {typeof(T2).Name}."),
-			(T3?)args.Parameters[index++].Evaluate() ?? throw new FormatException($"{callerName} third argument should be a {typeof(T3).Name}."),
-			(T4?)args.Parameters[index++].Evaluate() ?? throw new FormatException($"{callerName} fourth argument should be a {typeof(T4).Name}."),
-			(T5?)args.Parameters[index++].Evaluate() ?? throw new FormatException($"{callerName} fifth argument should be a {typeof(T5).Name}.")
+			(T1?)args.Parameters[0].Evaluate() ?? throw new FormatException($"{callerName} first argument should be a {typeof(T1).Name}."),
+			(T2?)args.Parameters[1].Evaluate() ?? throw new FormatException($"{callerName} second argument should be a {typeof(T2).Name}."),
+			(T3?)args.Parameters[2].Evaluate() ?? throw new FormatException($"{callerName} third argument should be a {typeof(T3).Name}."),
+			(T4?)args.Parameters[3].Evaluate() ?? throw new FormatException($"{callerName} fourth argument should be a {typeof(T4).Name}."),
+			(T5?)args.Parameters[4].Evaluate() ?? throw new FormatException($"{callerName} fifth argument should be a {typeof(T5).Name}.")
 			);
 	}
 
@@ -79,15 +75,14 @@ internal static class Parameters
 		[CallerMemberName] string callerName = "")
 	{
 		CheckParameterCount(6, 6, args.Parameters, callerName);
-		var index = 0;
 
 		return new Tuple<T1, T2, T3, T4, T5, T6>(
-			(T1?)args.Parameters[index++].Evaluate() ?? throw new FormatException($"{callerName} first argument should be a {typeof(T1).Name}."),
-			(T2?)args.Parameters[index++].Evaluate() ?? throw new FormatException($"{callerName} second argument should be a {typeof(T2).Name}."),
-			(T3?)args.Parameters[index++].Evaluate() ?? throw new FormatException($"{callerName} third argument should be a {typeof(T3).Name}."),
-			(T4?)args.Parameters[index++].Evaluate() ?? throw new FormatException($"{callerName} fourth argument should be a {typeof(T4).Name}."),
-			(T5?)args.Parameters[index++].Evaluate() ?? throw new FormatException($"{callerName} fifth argument should be a {typeof(T5).Name}."),
-			(T6?)args.Parameters[index++].Evaluate() ?? throw new FormatException($"{callerName} sixth argument should be a {typeof(T6).Name}.")
+			(T1?)args.Parameters[0].Evaluate() ?? throw new FormatException($"{callerName} first argument should be a {typeof(T1).Name}."),
+			(T2?)args.Parameters[1].Evaluate() ?? throw new FormatException($"{callerName} second argument should be a {typeof(T2).Name}."),
+			(T3?)args.Parameters[2].Evaluate() ?? throw new FormatException($"{callerName} third argument should be a {typeof(T3).Name}."),
+			(T4?)args.Parameters[3].Evaluate() ?? throw new FormatException($"{callerName} fourth argument should be a {typeof(T4).Name}."),
+			(T5?)args.Parameters[4].Evaluate() ?? throw new FormatException($"{callerName} fifth argument should be a {typeof(T5).Name}."),
+			(T6?)args.Parameters[5].Evaluate() ?? throw new FormatException($"{callerName} sixth argument should be a {typeof(T6).Name}.")
 			);
 	}
 
@@ -96,16 +91,15 @@ internal static class Parameters
 		[CallerMemberName] string callerName = "")
 	{
 		CheckParameterCount(7, 7, args.Parameters, callerName);
-		var index = 0;
 
 		return new Tuple<T1, T2, T3, T4, T5, T6, T7>(
-			(T1?)args.Parameters[index++].Evaluate() ?? throw new FormatException($"{callerName} first argument should be a {typeof(T1).Name}."),
-			(T2?)args.Parameters[index++].Evaluate() ?? throw new FormatException($"{callerName} second argument should be a {typeof(T2).Name}."),
-			(T3?)args.Parameters[index++].Evaluate() ?? throw new FormatException($"{callerName} third argument should be a {typeof(T3).Name}."),
-			(T4?)args.Parameters[index++].Evaluate() ?? throw new FormatException($"{callerName} fourth argument should be a {typeof(T4).Name}."),
-			(T5?)args.Parameters[index++].Evaluate() ?? throw new FormatException($"{callerName} fifth argument should be a {typeof(T5).Name}."),
-			(T6?)args.Parameters[index++].Evaluate() ?? throw new FormatException($"{callerName} sixth argument should be a {typeof(T6).Name}."),
-			(T7?)args.Parameters[index++].Evaluate() ?? throw new FormatException($"{callerName} seventh argument should be a {typeof(T7).Name}.")
+			(T1?)args.Parameters[0].Evaluate() ?? throw new FormatException($"{callerName} first argument should be a {typeof(T1).Name}."),
+			(T2?)args.Parameters[1].Evaluate() ?? throw new FormatException($"{callerName} second argument should be a {typeof(T2).Name}."),
+			(T3?)args.Parameters[2].Evaluate() ?? throw new FormatException($"{callerName} third argument should be a {typeof(T3).Name}."),
+			(T4?)args.Parameters[3].Evaluate() ?? throw new FormatException($"{callerName} fourth argument should be a {typeof(T4).Name}."),
+			(T5?)args.Parameters[4].Evaluate() ?? throw new FormatException($"{callerName} fifth argument should be a {typeof(T5).Name}."),
+			(T6?)args.Parameters[5].Evaluate() ?? throw new FormatException($"{callerName} sixth argument should be a {typeof(T6).Name}."),
+			(T7?)args.Parameters[6].Evaluate() ?? throw new FormatException($"{callerName} seventh argument should be a {typeof(T7).Name}.")
 			);
 	}
 
